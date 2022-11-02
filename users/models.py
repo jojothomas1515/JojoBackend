d1739cecb2ddbc3c9b158861b646fff307b745f1
Jojo Thomas <jojothomas1515@gmail.com>
2022-11-02 11:25:31 +0100

    done

--- users/models.py
+++ users/models.py
@@ -11,8 +11,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     profile_image = models.ImageField(_('Profile Image'), null='True', blank=True,
                                       default='https://firebasestorage.googleapis.com/v0/b/jojopage-001.appspot.com/o/default%2Fno-profile-picture-icon-11.jpg?alt=media&token=7f8f1d08-6ce9-4ef5-b00e-0939e70895ed',
-                                      upload_to=f'images/profile', max_length=200)
-    email = models.EmailField(_('email address'), unique=True)
+                                      upload_to=f'images/profile')
+    email = models.EmailField(_('email address'), unique=True, max_length=200,)
     firstname = models.CharField(_('First Name'), max_length=100, null=True, blank=True)
     lastname = models.CharField(_('Last Name'), max_length=100, null=True, blank=True)
     is_staff = models.BooleanField(default=False)
