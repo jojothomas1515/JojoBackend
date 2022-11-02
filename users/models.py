3fe321147b3e7c4b799b6077de34e43df6381ca8
Jojo <jojothomas1515@gmail.com>
2022-11-02 12:35:21 +0100

    i hope this fixes it

--- users/models.py
+++ users/models.py
@@ -7,17 +7,17 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(_('username'), unique=True, max_length=100, null=True, blank=True)
+    username = models.CharField(_('username'), unique=True, max_length=200, null=True, blank=True)
 
     profile_image = models.ImageField(_('Profile Image'), null='True', blank=True,
                                       default='https://firebasestorage.googleapis.com/v0/b/jojopage-001.appspot.com/o/default%2Fno-profile-picture-icon-11.jpg?alt=media&token=7f8f1d08-6ce9-4ef5-b00e-0939e70895ed',
                                       upload_to=f'images/profile')
-    email = models.EmailField(_('email address'), unique=True, max_length=200,)
+    email = models.EmailField(_('email address'), unique=True, max_length=200)
     firstname = models.CharField(_('First Name'), max_length=100, null=True, blank=True)
     lastname = models.CharField(_('Last Name'), max_length=100, null=True, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
+    date_joined = models.DateTimeField(default=timezone.now,max_length=200)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
