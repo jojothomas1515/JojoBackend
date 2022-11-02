f7f51e3fd3e985f9ae945cb801bad6d75e0a8ba2
Jojo Thomas <jojothomas1515@gmail.com>
2022-11-02 10:58:29 +0100

    dss

--- JojoPageBackend/settings.py
+++ JojoPageBackend/settings.py
@@ -90,8 +90,7 @@ WSGI_APPLICATION = 'JojoPageBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
 
@@ -202,3 +201,6 @@ elif DEBUG:
     STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static')]
     STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
